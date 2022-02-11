6222eced99c8427c9fccd80cd62beb9b09c7fccf
saito_yusuke <y.app.adm@gmail.com>
2022-02-11 10:16:19 +0900

    [feature] カウントアップして画面回転するとデータが保持されない(varバージョン)

--- app/src/main/java/yusuke/saito/using_viewmodel_incorrectly/MainActivity.kt
+++ app/src/main/java/yusuke/saito/using_viewmodel_incorrectly/MainActivity.kt
@@ -2,10 +2,21 @@ package yusuke.saito.using_viewmodel_incorrectly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import yusuke.saito.using_viewmodel_incorrectly.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+    private val viewModel = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.countUp.setOnClickListener {
+            viewModel.count = viewModel.count + 1
+            viewBinding.countView.text = viewModel.count.toString()
+        }
     }
 }
---
+++ app/src/main/java/yusuke/saito/using_viewmodel_incorrectly/MainViewModel.kt
@@ -0,0 +1,7 @@
+package yusuke.saito.using_viewmodel_incorrectly
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var count: Int = 0
+}
